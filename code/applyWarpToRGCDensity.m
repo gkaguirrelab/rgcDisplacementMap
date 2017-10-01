05bb6293047d008073ab0da62f6f6f6af95072b3
Michael Barnett <micalan@sas.upenn.edu>
2017-10-01 17:18:54 -0400

    comments

--- code/applyWarpToRGCDensity.m
+++ code/applyWarpToRGCDensity.m
@@ -1,3 +1,10 @@
+%% applyWarpToRGC
+% 
+% This script generates a 2D RGC map from looping over meridian angles with
+% getSplineFitToRGCDensity.m and applies the displacement map from 
+% makeDisplacementMap.m withte function warpImage.m
+%
+
 
 %% Set up params
 
@@ -8,6 +15,7 @@ xSmps = mapResolutionDegrees:mapResolutionDegrees:maxModeledEccentricity;
 
 
 %% make RGC Density Map
+
 angularResolutionDegrees = 15;
 angles = 0:angularResolutionDegrees:360-angularResolutionDegrees;
 
@@ -24,14 +32,14 @@ imR = PolarToIm (imP, 0, 1, imRdim, imRdim);
 rgcDensityMapDeg = imrotate(imR .* maxDensityDeg,-90);
 
 
-%% get displacement map
-% Optional anaysis params
+%% Get displacement map
 
 [ displacementMapDeg, fitParams, meridianAngles, rgcDisplacementEachMeridian, mRGC_cumulativeEachMeridian, mRF_cumulativeEachMeridian, convergenceEccen] = ...
     makeDisplacementMap('verbose',true,'maxModeledEccentricity',maxModeledEccentricity, 'displacementMapPixelsPerDeg',displacementMapPixelsPerDeg);
 
 
 %% Apply warp
+
 smps = linspace(-1*maxModeledEccentricity,maxModeledEccentricity,length(xSmps)*2);
 
 [sampleBaseX,sampleBaseY] = meshgrid(smps,smps);
