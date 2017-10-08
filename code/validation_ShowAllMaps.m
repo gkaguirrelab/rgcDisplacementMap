50212fb2acb1f31081cd5502bce4814122b415dc
gkaguirre <gkaguirre@me.com>
2017-10-08 12:11:52 -0400

    Set the default to 1 deg resolution
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- code/validation_ShowAllMaps.m
+++ code/validation_ShowAllMaps.m
@@ -9,7 +9,7 @@ p = inputParser; p.KeepUnmatched = true;
 % Optional anaysis params
 p.addParameter('sampleResolutionDegrees',0.01,@isnumeric);
 p.addParameter('maxModeledEccentricity',30,@isnumeric);
-p.addParameter('meridianAngleResolutionDeg',15,@isnumeric);
+p.addParameter('meridianAngleResolutionDeg',1,@isnumeric);
 p.addParameter('displacementMapPixelsPerDeg',10,@isnumeric);
 p.addParameter('pathToPlotOutputDir','~/Desktop/rgcDisplacementMapPlots',@ischar);
 
