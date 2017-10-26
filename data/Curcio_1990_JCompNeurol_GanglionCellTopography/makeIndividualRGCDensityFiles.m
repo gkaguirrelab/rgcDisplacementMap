f57c227643e06ec4ffa4dcf2b629748d2c704a13
gkaguirre <gkaguirre@me.com>
2017-10-26 15:53:48 -0400

    Continuing to improve data provenance
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- data/Curcio_1990_JCompNeurol_GanglionCellTopography/makeIndividualRGCDensityFiles.m
+++ data/Curcio_1990_JCompNeurol_GanglionCellTopography/makeIndividualRGCDensityFiles.m
@@ -1,27 +1,30 @@
 
-tableFileName = 'DENSITY5_gc_resorted_computedAverage.xls';
+tableFileNames = {'DENSITY5_gc_resorted_computedAverage.xls','curcioReportedGCAverage.xls'};
 outputFileNameStem = 'curcioRawRGCDensity_';
 
-T=readtable(tableFileName);
-
-SupportPosDeg=table2array(T(1,4:end));
-
-uniqueSubjectNames = unique(table2array(T(2:end,1)));
-
-meridianNames = {'NASAL','SUPERIOR','TEMPORAL','INFERIOR'};
-
-for nn=1:length(uniqueSubjectNames)
-    curcioRawRGCDensity = struct;
-    curcioRawRGCDensity.support = SupportPosDeg;
-    subjectName=uniqueSubjectNames{nn};
-    for mm = 1:length(meridianNames)
-        thisMeridianName = meridianNames{mm};
-    rowIdx=find( strcmp(table2array(T(:,1)),subjectName) .* strcmp(table2array(T(:,2)),thisMeridianName) );
-    curcioRawRGCDensity.(lower(thisMeridianName)) = table2array(T(rowIdx,4:end));
-    end
-    curcioRawRGCDensity.meta.subjectName = subjectName;
-    curcioRawRGCDensity.meta.dataTableName = tableFileName;
-    curcioRawRGCDensity.meta.densityUnits = 'counts/mm2';
-    curcioRawRGCDensity.meta.supportUnits = 'deg';
-    save([outputFileNameStem subjectName], 'curcioRawRGCDensity');
-end
+for tt = 1:length(tableFileNames)
+    T=readtable(tableFileNames{tt});
+    
+    support=table2array(T(1,4:end));
+    
+    uniqueSubjectNames = unique(table2array(T(2:end,1)));
+    
+    meridianNames = {'NASAL','SUPERIOR','TEMPORAL','INFERIOR'};
+    
+    for nn=1:length(uniqueSubjectNames)
+        curcioRawRGCDensity = struct;
+        curcioRawRGCDensity.support = support;
+        subjectName=uniqueSubjectNames{nn};
+        for mm = 1:length(meridianNames)
+            thisMeridianName = meridianNames{mm};
+            rowIdx=find( strcmp(table2array(T(:,1)),subjectName) .* strcmp(table2array(T(:,2)),thisMeridianName) );
+            curcioRawRGCDensity.(lower(thisMeridianName)) = table2array(T(rowIdx,4:end));
+        end
+        curcioRawRGCDensity.meta.subjectName = subjectName;
+        curcioRawRGCDensity.meta.dataTableName = tableFileNames{tt};
+        curcioRawRGCDensity.meta.densityUnits = T.Units{rowIdx};
+        curcioRawRGCDensity.meta.supportUnits = T.Units{1};
+        save([outputFileNameStem subjectName], 'curcioRawRGCDensity');
+    end % loop over subjects
+    
+end % loop over tables
--- data/Curcio_1990_JCompNeurol_HumanPhotoreceptorTopography/makeIndividualConeDensityFiles.m
+++ data/Curcio_1990_JCompNeurol_HumanPhotoreceptorTopography/makeIndividualConeDensityFiles.m
@@ -4,7 +4,7 @@ outputFileNameStem = 'curcioRawConeDensity_';
 
 T=readtable(tableFileName);
 
-SupportPosDeg=table2array(T(1,4:end));
+support=table2array(T(1,4:end));
 
 uniqueSubjectNames = unique(table2array(T(2:end,1)));
 
@@ -12,7 +12,7 @@ meridianNames = {'NASAL','SUPERIOR','TEMPORAL','INFERIOR'};
 
 for nn=1:length(uniqueSubjectNames)
     curcioRawConeDensity = struct;
-    curcioRawConeDensity.support = SupportPosDeg;
+    curcioRawConeDensity.support = support;
     subjectName=uniqueSubjectNames{nn};
     for mm = 1:length(meridianNames)
         thisMeridianName = meridianNames{mm};
@@ -21,7 +21,7 @@ for nn=1:length(uniqueSubjectNames)
     end
     curcioRawConeDensity.meta.subjectName = subjectName;
     curcioRawConeDensity.meta.dataTableName = tableFileName;
-    curcioRawConeDensity.meta.densityUnits = 'counts/mm2';
-    curcioRawConeDensity.meta.supportUnits = 'deg';
+            curcioRawConeDensity.meta.densityUnits = T.Units{rowIdx};
+        curcioRawConeDensity.meta.supportUnits = T.Units{1};
     save([outputFileNameStem subjectName], 'curcioRawConeDensity');
 end
