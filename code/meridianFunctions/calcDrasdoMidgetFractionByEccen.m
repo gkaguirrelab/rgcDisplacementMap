0c19c42251f073dc9554200fd58d7b45e7f834aa
gkaguirre <gkaguirre@me.com>
2017-10-26 23:02:33 -0400

    Clean up the drasdo midget fraction model to reflect clarity on retinal vs visual degrees
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- code/meridianFunctions/calcDrasdoMidgetFractionByEccen.m
+++
@@ -1,6 +0,0 @@
-function midgetFraction = calcDrasdoMidgetFractionByEccen(supportPosDeg,f0,rm)
-
-% The equation is taken from Drasdo et al 2007.
-midgetFraction = f0.*(1+(supportPosDeg./rm)).^-1;
-
-end
