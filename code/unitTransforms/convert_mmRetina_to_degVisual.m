e78ebe02854e76149509b50685ade3795e4fe551
gkaguirre <gkaguirre@me.com>
2017-10-28 14:19:08 -0400

    Implemented a geometric correction for optical vs. visual axis
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- code/unitTransforms/convert_mmRetina_to_degVisual.m
+++ code/unitTransforms/convert_mmRetina_to_degVisual.m
@@ -1,23 +1,85 @@
-function supportPosDegVisual = convert_mmRetina_to_degVisual(supportPosMmRetina)
+function supportPosDegVisualFieldRelativeToVisualAxis = convert_mmRetina_to_degVisual(supportPosMmRetinaRelativeToVisualAxis, polarAngle, varargin)
 % convert_mmRetina_to_degVisual
 %
 %   Converts mm on the retina from the visual axis to visual angle in
-%   degrees from the visual axis. It is based on Appendix 6 of Watson 2014,
-%   which in turn is derived from Drasdo & Fowler 1974.
+%   degrees from the visual axis. It is based on Appendix 6 of Watson 2014.
+%   The core of the conversion is from Drasdo & Fowler 1974. Drasdo &
+%   Fowler's equation relates position on the retina to position in the
+%   visual field in relation to the optic axis of the eye. Watson 2014
+%   observed that a correction (dependent on polar angle) is necessary to
+%   correct the conversion to be relative to the visual axis (which is
+%   otherwise assumed throughout this toolbox). Watson offered
+%   approximation to the correction; here we implement the full geometric
+%   correction.
 %
-% Input: 
-%   supportPosMm  = Retinal postion(s) in milimeters. Either scalar value or
-%                     vector input accepted.
 %
-% Output: 
-%   supportPosDeg = Retinal postion(s) in degrees
+% Input:
+%   supportPosMmRetinaRelativeToVisualAxis - retinal postion(s) in
+%       milimeters relative to visual axis of the eye. Either scalar value
+%       or vector input accepted.
+%   polarAngle - the polarAngle of the meridian for which the conversion
+%       should be calculated. An arbitrary value between 0-360 is accepted.
+%       (0=nasal;90=superior;180=temporal;270=inferior)
 %
-% Sample Call:
-%   supportPosMm  = 0:2:20;
-%   supportPosDeg = convert_deg_to_mm(supportPosMm);
+% Output:
+%   supportPosDegVisualFieldRelativeToVisualAxis
+%
+% Optional:
+%   displacementOpticalAxisFromVisualAlongNasalMeridianMm
+%   displacementOpticalAxisFromVisualAlongSuperiorMeridianMm - From Watson
+%       2014 (in turn from Charman 1991, quoting Emsley 1952), the optical
+%       axis intersects the retina 1.5 mm nasal and 0.5 mm superior to the
+%       visual axis.
 %
 
-supportPosDegVisual = 3.556.*supportPosMmRetina + 0.05593.*(supportPosMmRetina.^2) - 0.007358.*(supportPosMmRetina.^3) +0.0003027.*(supportPosMmRetina.^4);
 
+%% Parse input and define variables
+p = inputParser;
+
+% required input
+p.addRequired('supportPosMmRetinaRelativeToVisualAxis',@isnumeric);
+p.addRequired('polarAngle',@isnumeric);
+
+% Optional analysis params
+p.addParameter('displacementOpticalAxisFromVisualAlongNasalMeridianMm',1.5,@isnumeric);
+p.addParameter('displacementOpticalAxisFromVisualAlongSuperiorMeridianMm',0.5,@isnumeric);
+
+% parse
+p.parse(supportPosMmRetinaRelativeToVisualAxis,polarAngle,varargin{:})
+
+
+%% Adjust for displacement of the visual from the optical axis
+% check the input
+if polarAngle>360 || polarAngle<0
+    error('Provide polarAngle between 0 and 360 degrees');
+end
+
+% Determine polarAngle value for the vector that arises from the visual
+% axis and intersects the optical axis
+angleVisualToOpticalAxis = rad2deg(tan( p.Results.displacementOpticalAxisFromVisualAlongSuperiorMeridianMm / ...
+    p.Results.displacementOpticalAxisFromVisualAlongNasalMeridianMm));
+
+% Determine the length of the vector that arises from the visual axis and
+% intersects the optical axis
+distanceMmRetinaVisualToOpticalAxis = sqrt(p.Results.displacementOpticalAxisFromVisualAlongSuperiorMeridianMm^2 + ...
+    p.Results.displacementOpticalAxisFromVisualAlongNasalMeridianMm^2);
+
+% Convert distances from visual axis to distances from optical axis
+supportPosMmRetinaRelativeToOpticalAxis = ...
+    sqrt(...
+    (supportPosMmRetinaRelativeToVisualAxis-(distanceMmRetinaVisualToOpticalAxis.*cos(deg2rad(polarAngle - angleVisualToOpticalAxis)))).^2 + ...
+    (distanceMmRetinaVisualToOpticalAxis .* sin(deg2rad(polarAngle - angleVisualToOpticalAxis))).^2 ...
+    );
+
+%% Perform the calciulation
+supportPosDegVisualFieldRelativeToVisualAxis = ...
+    drasdoAndFowlerConversion(supportPosMmRetinaRelativeToVisualAxis - supportPosMmRetinaRelativeToOpticalAxis) + ...
+    drasdoAndFowlerConversion(supportPosMmRetinaRelativeToOpticalAxis);
+
+end % main function
+
+% Local Drasdo & Fowler equation
+function degreesVisualRelativeToOpticAxis = drasdoAndFowlerConversion(mmRetinaRelativeToOpticAxis)
+degreesVisualRelativeToOpticAxis = 3.556.*mmRetinaRelativeToOpticAxis + 0.05593.*(mmRetinaRelativeToOpticAxis.^2) - 0.007358.*(mmRetinaRelativeToOpticAxis.^3) +0.0003027.*(mmRetinaRelativeToOpticAxis.^4);
 end
 
