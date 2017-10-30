8a36dfb49e3d231e89e33d26532895c7e243cf94
gkaguirre <gkaguirre@me.com>
2017-10-30 00:07:13 -0400

    The displaced function is zero where it has not otherwise been assigned a value
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- code/meridianFunctions/applyDisplacement.m
+++ code/meridianFunctions/applyDisplacement.m
@@ -20,7 +20,7 @@ p.parse(valuesAtRGCSoma, displacementDegRetina, supportPosDegRetina, varargin{:}
 destination=round(supportPosDegRetina-displacementDegRetina,ceil(-log10(p.Results.sampleResolutionDegreesRetina)));
 destinationIdx=arrayfun(@(x) find((abs(supportPosDegRetina-x))<1e-6,1), unique(destination));
 valuesAtConesAtDestinations = arrayfun(@(x) sum(valuesAtRGCSoma(destination==x)), unique(destination));
-valuesAtCones=nan(size(valuesAtRGCSoma));
+valuesAtCones=zeros(size(valuesAtRGCSoma));
 valuesAtCones(unique(destinationIdx))=valuesAtConesAtDestinations;
 
 end
