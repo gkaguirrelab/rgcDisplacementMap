3be48a37d7ae7b56d52366bc3c169a877661dc69
gkaguirre <gkaguirre@me.com>
2017-10-29 22:55:47 -0400

    calcDisplacement now a stand-alone function
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- code/makeDisplacementMap.m
+++ code/makeDisplacementMap.m
@@ -299,27 +299,5 @@ withinRangeIdx = find(regularSupportPosDeg > targetConvergencePointDegRetina);
 error = sqrt(sum((countPerRingRGC(withinRangeIdx) - countPerRingRF(withinRangeIdx)).^2));
 end
 
-function displaceInDeg = calcDisplacement(regularSupportPosDeg, countPerRingRGC, countPerRingRF)
 
-% Determine the sample resolution by a difference operation
-tmp = diff(regularSupportPosDeg);
-sampleResolutionDegreesRetina = tmp(1);
-% Measure the displacement (in degrees). First, for each cumulative RGC
-% density value, identify the array index of the first
-% cumulative RF density value that is equal or greater.
-displaceInSamples=arrayfun(@(x) find(countPerRingRF>=x,1), countPerRingRGC,'UniformOutput',false);
-% Now some array operations to get these values out of cells and in to a
-% numeric vector
-emptyCells = find(cellfun(@(x) isempty(x), displaceInSamples));
-displaceInSamples(emptyCells)={NaN};
-displaceInSamples=cell2mat(displaceInSamples(cellfun(@(x) ~isempty(x), displaceInSamples)));
-% The displacement in array samples is the array index of each cumulative
-% RGC density measurement, minus the array index of the matching RF density
-% measurement. This difference is then multiplied by the sample resolution
-% to get the displacement in degrees.
-displaceInDeg = ((1:1:length(displaceInSamples))-displaceInSamples ) * sampleResolutionDegreesRetina;
-% Zero out negative values after the point of convergence
-displaceInDeg(find(displaceInDeg < 0,1):end)=0;
-
-end
 
