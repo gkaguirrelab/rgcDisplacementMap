0b57525fe621b038b913bf525f223ec7a7b91d0b
gkaguirre <gkaguirre@me.com>
2017-11-11 15:36:06 -0500

    Now handles the optic disc
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- code/meridianFunctions/applyDisplacement.m
+++ code/meridianFunctions/applyDisplacement.m
@@ -18,9 +18,11 @@ p.addParameter('sampleResolutionDegreesRetina',0.01,@isnumeric);
 p.parse(valuesAtRGCSoma, displacementDegRetina, supportPosDegRetina, varargin{:})
 
 destination=round(supportPosDegRetina-displacementDegRetina,ceil(-log10(p.Results.sampleResolutionDegreesRetina)));
-destinationIdx=arrayfun(@(x) find((abs(supportPosDegRetina-x))<1e-6,1), unique(destination));
-valuesAtConesAtDestinations = arrayfun(@(x) sum(valuesAtRGCSoma(destination==x)), unique(destination));
+uniqueDestinations = unique(destination);
+destinationIdx=arrayfun(@(x) find((abs(supportPosDegRetina-x))<1e-6,1), uniqueDestinations, 'UniformOutput', false);
+validDestinationIndices = cellfun(@(x) ~isempty(x), destinationIdx);
+valuesAtConesAtValidDestinations = arrayfun(@(x) sum(valuesAtRGCSoma(destination==x)), uniqueDestinations(validDestinationIndices));
 valuesAtCones=zeros(size(valuesAtRGCSoma));
-valuesAtCones(unique(destinationIdx))=valuesAtConesAtDestinations;
+valuesAtCones(unique(cell2mat(destinationIdx(validDestinationIndices))))=valuesAtConesAtValidDestinations;
 
 end
