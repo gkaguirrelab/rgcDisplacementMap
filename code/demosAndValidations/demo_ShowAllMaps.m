0a4ffa7aa2eb768b3826431e4fb47e9c732d3903
gkaguirre <gkaguirre@me.com>
2017-11-12 17:29:57 -0500

    Comment updates
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- code/demosAndValidations/demo_ShowAllMaps.m
+++ code/demosAndValidations/demo_ShowAllMaps.m
@@ -1,7 +1,18 @@
-function validation_ShowAllMaps(varargin)
-
-% Transform cone density map to RGC density map
-
+function demo_ShowAllMaps(varargin)
+% demo_ShowAllMaps - Model output in image maps
+%
+% This validation function runs the displacement model for each of many
+% meridian values and then converts the model output to images which are
+% displayed and optionally saved to disk. The analysis parameters are
+% explicitly defined here and then passed to the main,
+% createDisplacementModel routine.
+%
+% The "subjectName" corresponds to one of the Curcio 1990 datasets present
+% within the data directory of this toolbox. Typical options include:
+%   reportedAverage - the values reported in the Curcio 1990 papers
+%   computedAverage - our derivation of average values from the Curcio data
+%   29986A - data from subject 29986, averaged over both eyes.
+%   
 
 %% Parse input and define variables
 p = inputParser; p.KeepUnmatched = true;
@@ -11,7 +22,7 @@ p.addParameter('sampleResolutionDegreesRetina',0.01,@isnumeric);
 p.addParameter('maxModeledEccentricityDegreesRetina',30,@isnumeric);
 p.addParameter('meridianAngleResolutionDeg',1,@isnumeric);
 p.addParameter('displacementMapPixelsPerDegRetina',10,@isnumeric);
-p.addParameter('subjectName', 'reportedAverage', @ischar);
+p.addParameter('subjectName', 'computedAverage', @ischar);
 
 % Optional display and ouput params
 p.addParameter('verbose',true,@islogical);
