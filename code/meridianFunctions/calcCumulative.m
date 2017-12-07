308445dd4bf814957f8fde736835741e02ed533c
gkaguirre <gkaguirre@me.com>
2017-12-07 16:01:33 -0500

    Rename function for clarity
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- code/meridianFunctions/calcCumulative.m
+++
@@ -1,8 +0,0 @@
-
-function countsPerRing = calcCumulative(regularSupportPosDeg, densityFunction)
-
-extendedRegularSupportPosDeg = [regularSupportPosDeg (regularSupportPosDeg(end)+diff(regularSupportPosDeg(1:2)))];
-ringArea = diff(extendedRegularSupportPosDeg.^2 * pi);
-countsPerRing = cumsum(densityFunction.*ringArea);
-
-end
