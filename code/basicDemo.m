1f73e4b39a5ef203ba93879554417753d1c2c882
gkaguirre <gkaguirre@me.com>
2017-12-16 13:52:17 -0500

    Improving the comments
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- code/basicDemo.m
+++ code/basicDemo.m
@@ -1,21 +1,29 @@
-
+% basicDemo.m
+% Demonstrate the creation of a default RGC displacement map
 
 %% Create the default displacement model.
 % The variable rgcDisplacementByMeridian contains an m x p matrix, where m
 % is number of meridians and p is the number of modeled eccentricity
-% positions. The values are the displacement in retinal
-% degrees of the RGC soma away from the fovea. The next two variables are
-% the support for the m and p dimensions, respectively.
+% positions. The values specify the displacement of the RGC soma away from
+% the fovea in units of retinal degrees. The next two variables are the
+% support for the m and p dimensions, respectively.
 [rgcDisplacementByMeridian, meridianAngleSupport, regularSupportPosDegRetina] = ...
     createDisplacementModel('verbose',true);
 
 
 %% Convert from polar to image coordinates
+
 % Define the image sample base for transform from polar coords. We reduce
-% the resolution a bit to speed things up.
+% the resolution from the default of 0.01 retinal degrees per pixel to 10
+% per pixel to speed things up.
 displacementMapPixelsPerRetinalDeg = 10;
 maxModeledEccentricity = max(regularSupportPosDegRetina);
-imRdim = (maxModeledEccentricity * displacementMapPixelsPerRetinalDeg * 2) -1;
+imRdim = ...
+    (maxModeledEccentricity * displacementMapPixelsPerRetinalDeg * 2) -1;
+
+% This step actually makes the image map. If the 'imRdim' key/value pair is
+% omitted, the map is returned with the same spatial resolution as the
+% variable rgcDisplacementByMeridian. 
 rgcDisplacementMap = ...
     convertPolarMapToImageMap(rgcDisplacementByMeridian,'imRdim',imRdim);
 
@@ -23,7 +31,7 @@ rgcDisplacementMap = ...
 %% Display the map
 displayRetinalImage(...
     rgcDisplacementMap, ...                     % the image to be displayed
-    [0 4], ...                                  % min max of the color bar range
+    [0 ceil(max(max(rgcDisplacementMap)))], ... % min max of the color bar range
     displacementMapPixelsPerRetinalDeg, ...     % resolution of the image map
     maxModeledEccentricity, ...                 % the maximum eccentricity of the model
     'Displacement in retinal degrees');         % label for the color bar
